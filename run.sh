f057a2249a788afa8a5ccafa07a8133712409c53
laoidekc <s1430015@sms.ed.ac.uk>
2015-03-16 13:25:27 +0000

    Added run file correctly

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+python tsp.py 2 citiesAndDistances.pickled  outputs/output2.pickled
+python tsp.py 3 citiesAndDistances.pickled  outputs/output3.pickled
+python tsp.py 4 citiesAndDistances.pickled  outputs/output4.pickled
+python tsp.py 5 citiesAndDistances.pickled  outputs/output5.pickled
+python tsp.py 6 citiesAndDistances.pickled  outputs/output6.pickled
+python tsp.py 7 citiesAndDistances.pickled  outputs/output7.pickled
+python tsp.py 8 citiesAndDistances.pickled  outputs/output8.pickled
+python tsp.py 9 citiesAndDistances.pickled  outputs/output9.pickled
+python tsp.py 10 citiesAndDistances.pickled  outputs/output10.pickled
+python tsp.py 11 citiesAndDistances.pickled  outputs/output11.pickled
+python tsp.py 12 citiesAndDistances.pickled  outputs/output12.pickled
+python tsp.py 13 citiesAndDistances.pickled  outputs/output13.pickled
+python tsp.py 14 citiesAndDistances.pickled  outputs/output14.pickled
+
+python tests.py
